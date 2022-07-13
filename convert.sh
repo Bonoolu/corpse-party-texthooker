f077462195986dfc56766fa2b4611e8ad30f52b6
D-Dennis <64061670+D-Dennis@users.noreply.github.com>
2022-07-13 17:12:09 +0200

    make convert.sh a pure bash script

--- convert.sh
+++ convert.sh
@@ -1,7 +1,6 @@
-#!/bin/zsh
+#!/bin/bash
 # Converting the audio files from .wav to .opus reduces the total file size from 6.9G down to 366M
 for i in *.wav; do
-	ffmpeg -i "$i" -y -loglevel 16 "${i%.*}.opus";
-	printf "$(ls *.opus | wc -l)/9848 %.2f%%\\n" "$(( $(ls *.opus | wc -l) / 9848.0 * 100.0 ))";
+	ffmpeg -i "$i" -y "${i%.*}.opus";
 done
 
